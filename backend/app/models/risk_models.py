44984e34752bd1b42c5dca08e39e92c52e87f279
Meemom <jemima.silaen@gmail.com>
2025-10-25 17:07:27 -0400

    updated risk assessment model

--- backend/app/models/risk_models.py
+++ backend/app/models/risk_models.py
@@ -3,4 +3,5 @@ from pydantic import BaseModel
 class RiskAssessment(BaseModel):
     risk_score: float
     risk_level: str
-    summary: str
+    ai_reason: str
+    issues_detected: list[str]
--- backend/app/services/fraud_detection_service.py
+++ backend/app/services/fraud_detection_service.py
@@ -1,4 +1,14 @@
 from models.risk_models import RiskAssessment
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 async def assess_fraud_risk(transaction_data: dict) -> RiskAssessment:
-    
+     return RiskAssessment(
+        risk_score="",
+        risk_level="",
+        ai_reason="",
+        issues_detected=[],
+    )
