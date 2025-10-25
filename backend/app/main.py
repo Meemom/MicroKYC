e758fbbd6810d09a957626029aae282819070a85
Meemom <jemima.silaen@gmail.com>
2025-10-25 15:54:17 -0400

    changed main name

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from app.routes import verify, analytics
 
-app = FastAPI(title="MicroKYC API", version="1.0")
+app = FastAPI(title="gitIT API", version="1.0")
 
 app.include_router(verify.router, prefix="/verify", tags=["Verification"])
 app.include_router(analytics.router, prefix="/analytics", tags=["Analytics"])
 
 @app.get("/")
 def root():
-    return {"message": "MicroKYC backend is running"}
+    return {"message": "gitIT backend is running"}
